860d422ba7b724b6aa32d00527decbf8223ac970
Zoltan Fabian <77056471+FZoltanI@users.noreply.github.com>
2024-11-15 08:28:37 +0100

    new genre

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\GenreController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/new-genre', [GenreController::class,'create'])->name('create_genre');
